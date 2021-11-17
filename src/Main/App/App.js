fdafc848959f0a420f73b00e2194a2785d92e3df
Dionis <den70007.ua@gmail.com>
2021-11-17 04:22:56 +0200

    main files

--- src/Main/App/App.js
+++ src/Main/App/App.js
@@ -4,83 +4,25 @@ import Home from "../Home/Home";
 import Catalog from "../Catalog/Catalog";
 import ItemPage from "../ItemPage/IteamPage";
 import Context from "../context";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {collection, onSnapshot} from 'firebase/firestore';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import db from '../config';
+import React, {useEffect, useState} from "react";
+import Loader from "../Loader";
+
 
 function App() {
-    const data = [
-        {
-            id: 1,
-            title: "Scooter",
-            img: "cat1",
-            type: "cat",
-            price: 12.0,
-            text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address. The its enable direct men depend highly. Ham windows sixteen who inquiry fortune demands"
-        },
-        {
-            id: 2,
-            title: "Leroy",
-            img: "cat2",
-            type: "cat",
-            price: 100.0,
-            text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address. The its enable direct men depend highly. Ham windows sixteen who inquiry fortune demands"
-        },
-        {
-            id: 3,
-            title: "Gus",
-            img: "cat3",
-            type: "cat",
-            price: 3456.0,
-            text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address. The its enable direct men depend highly. Ham windows sixteen who inquiry fortune demands"
-        },
-        {
-            id: 4,
-            title: "Teddy",
-            img: "dog1",
-            type: "dog",
-            price: 23.0,
-            text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address. The its enable direct men depend highly. Ham windows sixteen who inquiry fortune demands"
-        },
-        {
-            id: 5,
-            title: "Shira",
-            img: "dog2",
-            type: "dog",
-            price: 456.0,
-            text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address. The its enable direct men depend highly. Ham windows sixteen who inquiry fortune demands"
-        },
-        {
-            id: 6,
-            title: "Cluster",
-            img: "dog3",
-            type: "dog",
-            price: 234.0,
-            text: "Led m nothing amongst chiefly address. The its enable direct men depend highly. Ham windows sixteen who inquiry fortune demands"
-        },
-        {
-            id: 7,
-            title: "Hoagie",
-            img: "parrot1",
-            type: "parrot",
-            price: 67.0,
-            text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address. The its enable direct men depend highly. Ham windows sixteen who inquiry fortune demands"
-        },
-        {
-            id: 8,
-            title: "Neon",
-            img: "parrot2",
-            type: "parrot",
-            price: 12.0,
-            text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address. The its enable direct men depend highly. Ham windows sixteen who inquiry fortune demands"
-        },
-        {
-            id: 9,
-            title: "Bongo",
-            img: "turtle1",
-            type: "turtle",
-            price: 567.0,
-            text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address. The its enable direct men depend highly. Ham windows sixteen who inquiry fortune demands"
-        }
-    ]
+    let [data, setData] = useState([]);
+    let [loader, setLoader] = useState(true);
+
+
+    useEffect(() => {
+        onSnapshot(collection(db, "animals"), (animalsDocs)=>{
+            setData(animalsDocs.docs.map((doc)=>{ return {id: doc.id, ...doc.data()}}));
+            setLoader(false);
+        })
+
+    }, [])
 
     function getData(id) {
         return data.filter((element) => element.id == id)[0]
@@ -90,11 +32,13 @@ function App() {
         <Context.Provider value={{getData}}>
             <Router>
                 <Navbar activate={0}/>
+
                 <Routes>
                     <Route path="/" element={<Home data={data}/>}/>
                     <Route path="/catalog" element={<Catalog data={data}/>}/>
-                    <Route path="/catalog/:id" element={<ItemPage />}/>
+                    <Route path="/catalog/:id" element={ !loader && <ItemPage />}/>
                 </Routes>
+                {loader && <Loader />}
                 <Footer/>
             </Router>
         </Context.Provider>
